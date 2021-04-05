b3403c25ff4b51f647f1630f32424ec410bc6d52
Andres <afelro97@gmail.com>
2021-04-05 18:12:06 -0500

    Cambio de pantalla

--- app/src/main/java/com/example/primeraappcalculadora/MainActivity.java
+++ app/src/main/java/com/example/primeraappcalculadora/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.primeraappcalculadora;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -39,6 +40,9 @@ public class MainActivity extends AppCompatActivity {
                 int number2 = Integer.parseInt(num2.getText().toString());
                 int sum = number1 + number2;
                 result.setText("Answer: " + String.valueOf(sum));
+
+                Intent changeScreen = new Intent(MainActivity.this, SecondScreenActivity.class);
+                startActivity(changeScreen);
             }
         });
 
--- app/src/main/java/com/example/primeraappcalculadora/SecondScreenActivity.java
+++ app/src/main/java/com/example/primeraappcalculadora/SecondScreenActivity.java
@@ -3,12 +3,28 @@ package com.example.primeraappcalculadora;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class SecondScreenActivity extends AppCompatActivity {
+
+    private Button secondScreenButton1;
+    private TextView secondScreenText1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second_screen);
+
+        secondScreenButton1 = findViewById(R.id.secondScreenButton1);
+        secondScreenText1 = findViewById(R.id.secondScreenText1);
+
+        secondScreenButton1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
     }
 }
